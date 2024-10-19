310fc951f86600d31b57829abd9faf36b0716871
laurinbrechter <brechterlaurin@gmail.com>
2024-10-19 16:24:28 +0200

    match darija when searching for example sents

--- README.md
+++ README.md
@@ -1,9 +1,11 @@
 
 ## Darija Dictionary/Translator.
 
-- Since I am currently learning some maroccan arabic (aka darija) I decided to build this dictionary/translator app.
+- Since I am currently learning some maroccan arabic (aka darija) and didn't find any translator/dictionary website for darija (that's also free beyond a limited amount of queries) I decided to build this dictionary/translator app.
 - It is based on the [darija-open-dataset](https://github.com/darija-open-dataset) that contains ~3000 words in darija
     - The dictionary is not comprehensive and there might be some mistakes as I simply took this dataset as the ground truth
+    - it is nonetheless the [largest open source darija-english dataset](https://arxiv.org/pdf/2103.09687)
+    - if you are a (native) speaker feel free to contribute to this project to help language learners.
 - additionally, there are around 40,000 example sentences that will be shown when you look up a word.
     - since the dataset is not comprehensive, the example sentences might contain words that are not in the dictionary itself.
 - the app is mostly aimed at language learners.
@@ -12,14 +14,20 @@
 
 ### Features
 
-- search for a word in darija/english and get autocompletions in the other language
-- once you found the word you are looking for, you get all the forms of the word and its example sentences
+- Current
+    - search for a word in darija/english and get autocompletions in the other language
+    - once you found the word you are looking for, you get all the forms of the word and its example sentences
+    - you will also get the various meanings of the word if there are multiple
+        - e.g. bzzaf
+- Planned
+    - train a model/LLM on the example sentences to allow for translations
+
 
 
 ### Technical
 
 - the app is written with nextjs, tailwindcss and shadcn/ui.
-- I am using a simple SQlite database for storing the raw dictionary as the data is static anyways and not sensitive.
+- I am using a simple Sqlite database for storing the raw dictionary as the data is static anyways and not sensitive.
     - this also means that we have no other components that we need to deploy seperately (e.g. a postgres database)
 - all pages for the different words are statically generated at build time and therefore server-site generated (SSG)
 - you can simply deploy this app for free using vercel
--- src/app/[lang]/[word]/page.tsx
+++ src/app/[lang]/[word]/page.tsx
@@ -1,39 +1,32 @@
 import "dotenv/config";
 import { drizzle } from "drizzle-orm/libsql";
-import { InferSelectModel } from "drizzle-orm";
+import { and, InferSelectModel } from "drizzle-orm";
 import { exampleSents, wordsTable } from "@/db/schema";
 
-import { like } from "drizzle-orm";
-import { ScrollArea } from "@/components/ui/scroll-area"
+import { like, or } from "drizzle-orm";
+import { ScrollArea } from "@/components/ui/scroll-area";
 import { WordDetails } from "@/components/WordDetails";
 import searchAction from "@/actions/query";
 import SearchForm from "@/components/SearchForm";
 
-
-
 // This function gets called at build time
 export async function generateStaticParams() {
-
   const db = drizzle(process.env.DB_FILE_NAME!);
   const words = await db.select().from(wordsTable);
 
   // Get the paths we want to pre-render based on posts
-  const paths = words.map((word) => (
-    { word: word.n1, lang: 'darija' }
-))
- 
+  const paths = words.map((word) => ({ word: word.n1, lang: "darija" }));
+
   // We'll pre-render only these paths at build time.
   // { fallback: false } means other routes should 404.
-  return paths
+  return paths;
 }
-
-
 
 const Chat = async ({
   params,
-  // searchParams,
-}: {
-  params: { word: string, lang: 'darija' | 'english' };
+}: // searchParams,
+{
+  params: { word: string; lang: "darija" | "english" };
   // searchParams: { ctx: string[] | undefined | string };
 }) => {
   const db = drizzle(process.env.DB_FILE_NAME!);
@@ -42,24 +35,35 @@ const Chat = async ({
 
   const words = await searchAction(params.word, params.lang);
 
-
   const sentences: InferSelectModel<typeof exampleSents>[][] = [];
 
   for (const word of words) {
-    const sent = await db.select().from(exampleSents).where(like(exampleSents.eng, `%${word.eng}%`)).limit(10);
+    const sent = await db
+      .select()
+      .from(exampleSents)
+      .where(and(
+        like(exampleSents.eng, `%${word.eng}%`),
+        like(exampleSents.darija, `%${word.n1}%`)
+      ))
+      .limit(10);
     sentences.push(sent);
   }
 
   return (
     <>
-    <SearchForm />
-    <ScrollArea className="h-[400px] w-[60vw]">
-      {words.map((word, index) => {
-        return (
-          <WordDetails key={index} searchResults={word} language="darija" examples={sentences[index]} />
-        );
-      })}
-    </ScrollArea>
+      <SearchForm />
+      <ScrollArea className="h-[400px] w-[60vw]">
+        {words.map((word, index) => {
+          return (
+            <WordDetails
+              key={index}
+              searchResults={word}
+              language="darija"
+              examples={sentences[index]}
+            />
+          );
+        })}
+      </ScrollArea>
     </>
   );
 };
